8e3d1757984b1c979a7d0a3ece6ee70e8241ea4f
Nicolae Matei <nmatei@sdl.com>
2019-12-21 01:08:56 +0200

    salus logs

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# salus-logs
-collect logs from https://eu.salusconnect.io/dashboard
+# Salus Logs
+
+Collect logs from [https://eu.salusconnect.io](https://eu.salusconnect.io/dashboard)
+
+
+## For each Termostat create 2 OneTouch rules for ON & OFF to send an email
+
+- Rules name must be in following format "log-h-living-on" & "log-h-living-off"
+  change change "living" with your rooms.
+- content to send can be for example "{log-heating-living-on}" & "{log-heating-living-off}" - or any details you want.
+
+![log-h-living-on](screens/log-h-living-on.jpg)
+
+![log-h-living-off](screens/log-h-living-off.jpg)
+
+
+## in Gmail mark this emails as not importants and to skip inbox
+
+- open gmail Settings / Filters and Blocked Addresses
+- create a new Filter:
+    - Matches: from:(support@sc-smarthome.io OR support@salusconnect.io) subject:(OneTouch rule log-)
+    - Do this: Skip Inbox, Never send it to Spam, Never mark it as important
+
+## Import and "Salus-Controls.xlsx" in Google Drive
+
+Import [Salus-Controls.xlsx](Salus-Controls.xlsx) as google sheet
+...TODO...
+
+### Adjust summary view (second sheet)
+
+Multiply rooms as needed.
+
+### Open Tools/Script editor
+
+- copy content of [script.js](script.js) and paste it inside "Script editor"
+- press "Current project triggers" button (clock icon)
+    - select collectLogs() as function to run
+    - Time driven & hour timer & Every 2 hours
+
+![trigger](screens/run-trigger.jpg)
---
+++ script.js
@@ -0,0 +1,91 @@
+/**
+ * Run collectLogs every {x} hours 
+ */
+function collectLogs() {
+  var logs = getLogsFromMail();
+  if (logs.length) {
+    //var sheet = SpreadsheetApp.getActiveSheet();
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("logs");
+    
+    logs.forEach(function(log) {
+      //sheet.insertRowAfter(1);
+      sheet.insertRowBefore(2);
+      
+      var insertAt = 2,
+        last = sheet.getLastRow() + 1,
+        subject = log.subject,
+        room = subject.replace(/.*log-h-(\w*)-.*/gi, "$1"),
+        status = subject.indexOf("-off") >= 0 ? "OFF" : "ON";
+
+      sheet.getRange(insertAt, 1).setValue(room);
+      sheet.getRange(insertAt, 2).setValue(status);
+      sheet.getRange(insertAt, 3).setValue(log.date);
+
+      if (status === "OFF") {
+        // if the last record is also OFF
+        //  don't show the calculate time - it shows incorrect start time.
+        var lastOff = "=maxifs($C$3:$C" + last + ", $A$3:$A" + last + ", $A" + insertAt + ", $B$3:$B" + last + ', "OFF")';
+        sheet.getRange(insertAt, 4).setValue(lastOff);
+        var lastOFFTime = new Date(sheet.getRange(insertAt, 4).getDisplayValue());
+
+        var valueFormula = "=maxifs($C$2:$C" + last + ", $A$2:$A" + last + ", $A" + insertAt + ", $B$2:$B" + last + ', "ON")';
+        sheet.getRange(insertAt, 4).setValue(valueFormula);
+        sheet.getRange(insertAt, 5).setValue("=C" + insertAt + "-D" + insertAt);
+
+        var startTime = new Date(cleanFormula(sheet, insertAt, 4));
+        if (lastOFFTime.getTime() > startTime.getTime()) {
+          // missing ON (2 OFF records wihtout start between)
+          sheet.getRange(insertAt, 5).setValue("00:00:00.000");
+        } else {
+          cleanFormula(sheet, insertAt, 5);
+        }
+      }
+    });
+  }
+}
+
+/**
+ * @private
+ */
+function getLogsFromMail() {
+  var logs = [];
+  var query = "from:(support@sc-smarthome.io) subject:(OneTouch rule log-h-) -in:trash"; // is:unread
+  var threads = GmailApp.search(query);
+
+  for (var i = 0; i < threads.length; i++) {
+    var messages = threads[i].getMessages();
+    for (var j = messages.length - 1; j >= 0; j--) {
+      var mail = messages[j];
+
+      if (mail.isInTrash()) {
+        // STOP
+        j = -1;
+      } else {
+        var subject = mail.getSubject();
+
+        logs.push({
+          subject: subject,
+          date: mail.getDate()
+        });
+
+        mail.markRead();
+        mail.moveToTrash();
+      }
+    }
+  }
+  
+  logs.sort(function(a, b) {
+    return a.date.getTime() - b.date.getTime();
+  });
+  return logs;
+}
+
+/**
+ * @private
+ * clean formulas to improve display performance
+ */
+function cleanFormula(sheet, rowIdx, cellIdx) {
+  var value = sheet.getRange(rowIdx, cellIdx).getDisplayValue();
+  sheet.getRange(rowIdx, cellIdx).setValue(value);
+  return value;
+}
